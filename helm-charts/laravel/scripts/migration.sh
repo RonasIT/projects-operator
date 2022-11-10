a676276c48b80225984ed4cd4b2a1089f2ac3498
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2022-11-10 17:02:23 +0300

    feat(laravel): add new migration approach

---
+++ helm-charts/laravel/scripts/migration.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function migrate_status() {
+  php artisan migrate:status | tr '[:space:]' '[\n*]' | grep -i -c Pending
+}
+
+function migrate() {
+  php artisan migrate
+}
+
+function rollback() {
+   php artisan migrate:rollback --step $1
+}
+
+INITIAL_COUNT_PENDING_MIGRATIONS=$(migrate_status)
+
+(
+  migrate
+)
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  COUNT_PENDING_MIGRATIONS_AFTER_MIGRATE=$(migrate_status)
+
+  if [ "$INITIAL_COUNT_PENDING_MIGRATIONS" -ne "$COUNT_PENDING_MIGRATIONS_AFTER_MIGRATE" ]; then
+    ROLLBACK_STEP=$(expr $INITIAL_COUNT_PENDING_MIGRATIONS - $COUNT_PENDING_MIGRATIONS_AFTER_MIGRATE)
+    rollback $ROLLBACK_STEP
+  fi
+
+  exit $errorCode
+fi
